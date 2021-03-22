6a30e6137e215071d1855bd6ab5f56f56dabb358
DevDuki <dusan-misic@hotmail.com>
2021-03-22 17:47:43 +0100

    Zug gemeinde Daten sammeln (in progress)

--- index.js
+++ index.js
@@ -11,12 +11,44 @@ const app = express()
 app.get('/', async (request, response) => {
 
  
-  //const covidResponse = await fetch('https://covid19-rest.herokuapp.com/api/openzh/v1/country/CH/area/ZG')
-  const covidResponse = await fetch('https://www.covid19.admin.ch/de/overview#/definitions/DailyIncomingData/properties/geoRegion/ZG')
+  const covidResponse = await fetch('https://covid19-rest.herokuapp.com/api/openzh/v1/country/CH/area/ZG')
+  // const covidResponse = await fetch('https://www.covid19.admin.ch/de/overview')
   const data = await covidResponse.json()
 
+  const municipalities = [{Canton: "Zug", Ratio: 0.24}, {Canton: "Oberägeri", Ratio: 0.05}, {Canton: "Unterägeri", Ratio: 0.069}, {Canton: "Menzingen", Ratio: 0.035}, {Canton: "Baar", Ratio: 0.193}, {Canton: "Cham", Ratio: 0.132}, {Canton: "Hünenberg", Ratio: 0.069}, {Canton: "Steinhausen", Ratio: 0.079}, {Canton: "Risch", Ratio: 0.086}, {Canton: "Walchwil", Ratio: 0.03}, {Canton: "Neuheim", Ratio: 0.017}]
+
+
+
+
+  let cleanData = []
+
+
+  municipalities.forEach(municipality => {
+    data.records.forEach((record, index) => {
+
+      let cases = 0
+      if(index > 0){
+        cases = data.records[index].ncumul_conf - data.records[index-1].ncumul_conf * municipality.Ratio
+      }
+      
   
-  console.log('response', data)
+      const casesPerDay = {
+        municipality: municipality.Canton,
+        date: record.date,
+        cases: Math.floor(cases)
+      }
+  
+      
+  
+      cleanData.push(casesPerDay)
+    })
+  })
+  
+
+  
+  console.log('casesPerDay',cleanData)
+
+  response.json(cleanData)
 
   // const ZGDataForOtherStudents = {...data}
   // console.log(data)
@@ -29,7 +61,7 @@ app.get('/', async (request, response) => {
     {
        "bfsNr":     4001, 
        "date":      "2021-02-01",
-       "incidence": 17.5  
+       "incidence": 17.5
     },
   ]
   */  
